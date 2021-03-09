b41030a19469af17b618226ae3e0cb44229c0cda
krj8 <krjonson@gmail.com>
2021-03-08 19:14:13 -0800

    setting homepage layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Navbar from './components/Navbar';
 import TopNavbar from './components/TopNavbar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
@@ -8,12 +7,17 @@ function App() {
   return (
     <>
       <Router>
-          <TopNavbar/>
-          {/* <LeftNavbar /> */}
-          {/* <RightNavbar /> */}
-          <Switch>
-            <Route path='/' exact />
-          </Switch>
+        {/* <body  className="grid"> */}
+        <TopNavbar/>
+          <div className="background">
+              
+              {/* <LeftNavbar /> */}
+              {/* <RightNavbar /> */}
+              <Switch>
+                <Route path='/' exact />
+              </Switch>
+            </div>
+          {/* </body> */}
       </Router>
     </>
   );
