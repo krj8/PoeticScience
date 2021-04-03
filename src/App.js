d92a3ebe04cb4240de43b75d273fe3d10a9866d9
krj8 <krjonson@gmail.com>
2021-04-03 11:31:37 -0700

    Debugged, Unscrammbled file structure
    App works again

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import LeftMenu from './components/LeftMenu.jsx';
 import RightMenu from './components/RightMenu.jsx';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+// import Home from './components/Pages/Home/Home.js';
+
 
 function App() {
   return (
@@ -29,5 +31,29 @@ function App() {
     </>
   );
 }
+
+// function App() {
+//   return (
+//     <>
+//       <Router>
+//         <TopNavbar/>
+//           <body>
+//               <div className="background">
+//                   {/* <Contents /> */}
+//                   <div className='leftMenu'>
+//                     <LeftMenu />
+//                   </div>
+//                   <div className='rightMenu'>
+//                     <RightMenu />
+//                   </div>
+//                   <Switch>
+//                     <Route path='/' exact component={Home}/>
+//                   </Switch>
+//                 </div>
+//             </body>
+//       </Router>
+//     </>
+//   );
+// }
 
 export default App;
---
+++ src/components/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import '/Users/kindrajonson/Desktop/PoeticScience/website_files/React/poetic_science/src/App.css';
+// import AdaSection from '/Users/kindrajonson/Desktop/PoeticScience/website_files/React/poetic_science/src/components/Ada.js';
+
+// export default function Home() {
+//     return (
+//         <>
+//             <AdaSection/>
+//         </>
+//     );
+// }
