1d9920bb8407effb254180c1068da9433284e264
krj8 <krjonson@gmail.com>
2021-04-05 23:05:02 -0700

    learning single page application

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -14,7 +14,7 @@
 
 
 
-// Listens for fragment identifier to change. Loads new content for Single Page App
-window.addEventListener("hashchange", function() {
-    console.log(location.hash);
-});
+// // Listens for fragment identifier to change. Loads new content for Single Page App
+// window.addEventListener("hashchange", function() {
+//     console.log(location.hash);
+// });
