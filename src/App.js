49a499c1c7925cd773b5d21749e33504bac8e22b
krj8 <krjonson@gmail.com>
2021-04-11 18:43:01 -0700

    Stringing together paths for SPA

--- src/App.js
+++ src/App.js
@@ -1,38 +1,131 @@
 import React from 'react';
-import TopNavbar from './components/TopNavbar';
-// import Contents from './components/Contents.js';
-import LeftMenu from './components/LeftMenu.jsx';
-import RightMenu from './components/RightMenu.jsx';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
-// import Home from './components/Pages/Home/Home.js';
 
+// Use AppFrame as environment/background
+// import AppFrame from './AppFrame.js';
+import Home from './components/Home/Home.js';
 
-function App() {
+export default function App() {
   return (
-      <Router>
-        <TopNavbar/>
-          <body>
-              <div className="background">
-                  {/* <Contents /> */}
-                  <div className='leftMenu'>
-                    <LeftMenu />
-                  </div>
-                  <div className='rightMenu'>
-                    <RightMenu />
-                  </div>
-                  <Switch>
-                    <Route path='/' exact />
-                  </Switch>
-                </div>
-            </body>
-      </Router>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
+// ----------------------
+// import {
+//   Route,
+//    NavLink,
+//    HashRouter
+// } from 'react-router-dom';
+// import TopNavbar from './components/TopNavbar';
+// import LeftMenu from './components/LeftMenu.jsx';
+// import RightMenu from './components/RightMenu.jsx';
+// import Home from './components/Pages/Home.js';
+
+// class Main extends Component {
+//   render() {
+//     return (
+//       // define "routing region" with hash router component
+//       <HashRouter>
+//         <TopNavbar/>
+//           <body>
+//               <div className="background">
+
+//                 <div className='leftMenu'>
+//                   <LeftMenu />
+//                 </div>
+
+//                 <div className='rightMenu'>
+//                   <RightMenu />
+//                 </div>
+                
+//               </div>
+//           </body>
+//       </HashRouter>      
+//     );
+//   }
+// }
+
+// export default Main;
+
+
+
+
+
+
+
+
+
+
+
+// ---------------------------------
+// import React, { Component } from 'react';
+// import {
+//   Route,
+//    NavLink,
+//    HashRouter
+// } from 'react-router-dom';
+// import TopNavbar from './components/TopNavbar';
+// import LeftMenu from './components/LeftMenu.jsx';
+// import RightMenu from './components/RightMenu.jsx';
+// import Home from './components/Pages/Home.js';
+// // import Portfolio from;
+// // import Resume from ;
+// // import FrontCover from ;
+// // import Flyology from ;
+// // import Babbage from ;
+// // import BackCover from ;
+
+// class Main extends Component {
+//   render() {
+//     return (
+//       // define "routing region" with hash router component
+//       <HashRouter>
+//         <TopNavbar/>
+//           <body>
+//               <div className="background">
+
+//                 <div className='leftMenu'>
+//                   <LeftMenu />
+//                 </div>
+
+//                 <div className='rightMenu'>
+//                   <RightMenu />
+//                 </div>
+                
+//               </div>
+//           </body>
+//       </HashRouter>      
+//     );
+//   }
+// }
+
+// export default Main;
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import TopNavbar from './components/TopNavbar';
+// // import Contents from './components/Contents.js';
+// import LeftMenu from './components/LeftMenu.jsx';
+// import RightMenu from './components/RightMenu.jsx';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import './App.css';
+// // import Home from './components/Pages/Home/Home.js';
+
+
 // function App() {
 //   return (
-//     <>
 //       <Router>
 //         <TopNavbar/>
 //           <body>
@@ -45,13 +138,12 @@ function App() {
 //                     <RightMenu />
 //                   </div>
 //                   <Switch>
-//                     <Route path='/' exact component={Home}/>
+//                     <Route path='/' exact />
 //                   </Switch>
 //                 </div>
 //             </body>
 //       </Router>
-//     </>
 //   );
 // }
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './App.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App.js';
+
+ReactDOM.render(
+    <App/>, 
+    document.getElementById('root')
+);
